bdcdeef2f9d949780d62f14ec162e857130b072a
Mark David Teo <mark@192-168-1-104.tpgi.com.au>
2018-01-09 13:36:04 +0800

    bar chart is working

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { VictoryBar } from 'victory-native'
+import { Card } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Card >
+          <Text>VictoryBar</Text>
+          <VictoryBar />
+        </Card>
+        <Card style={{width:20}}>
+        </Card>
       </View>
     );
   }
