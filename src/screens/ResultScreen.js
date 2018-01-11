66348d86384e774d18cd89212783080ca11fc58d
Mark David Teo <mark@192-168-1-104.tpgi.com.au>
2018-01-11 16:10:33 +0800

    got d3 working using import star

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import * as shape from 'd3-shape';
+import * as scale from 'd3-scale';
+
+const d3 = {
+  scale,
+  shape,
+}
 
 // data represents the distribution of spendings in a month
 data = [
@@ -9,11 +16,17 @@ data = [
   {"number": 23, "name": 'Car'},
   {"number": 42, "name": 'Rent'},
   {"number":  4, "name": 'Misc'},
-];
+]
 
 class ResultScreen extends React.Component {
   componentDidMount(){
+    var arc = d3.shape.arc()
+    .innerRadius(0)
+    .outerRadius(100)
+    .startAngle(0)
+    .endAngle(Math.PI / 2);
     console.log(`hello world`)
+    console.log('arc:', arc())
   }
   render() {
     return (
