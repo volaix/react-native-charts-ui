e645c92a5197746a19e403f7d9c992d817857e3a
Mark David Teo <mark@192-168-1-104.tpgi.com.au>
2018-01-14 19:12:00 +0800

    inserted art for react native

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ART, Text, View } from 'react-native'
 import * as shape from 'd3-shape';
 import * as scale from 'd3-scale';
 
@@ -8,30 +8,45 @@ const d3 = {
   shape,
 }
 
-// data represents the distribution of spendings in a month
-data = [
-  {"number":  8, "name": 'Fun activities'},
-  {"number": 7, "name": 'Dog'},
-  {"number": 16, "name": 'Food'},
-  {"number": 23, "name": 'Car'},
-  {"number": 42, "name": 'Rent'},
-  {"number":  4, "name": 'Misc'},
-]
+const {
+  Surface,
+  Group,
+  Shape,
+} = ART;
 
 class ResultScreen extends React.Component {
-  componentDidMount(){
+  componentDidMount() {
     var arc = d3.shape.arc()
-    .innerRadius(0)
-    .outerRadius(100)
-    .startAngle(0)
-    .endAngle(Math.PI / 2);
-    console.log(`hello world`)
+      .innerRadius(0)
+      .outerRadius(100)
+      .startAngle(0)
+      .endAngle(Math.PI / 2);
+
     console.log('arc:', arc())
+
   }
   render() {
     return (
       <View>
+
         <Text>New Charts go here</Text>
+
+        <Surface 
+        width={1000} 
+        height={600} 
+        style={{ backgroundColor:'lightgrey' }}
+       >
+          <Group>
+          <Shape
+            d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+            stroke="#000"
+            strokeWidth={1}
+            />
+          </Group>
+        </Surface>
+
+        <Text>Text under surface</Text>
+
       </View>
     )
   }
